5fa9fe55da507a6698e845e1394e693a96e50dce
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-13 19:27:20 +0300

    finish authentication

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ use App\Http\Controllers\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
 });
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
