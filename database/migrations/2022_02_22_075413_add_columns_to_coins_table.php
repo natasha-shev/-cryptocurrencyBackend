f89ee9d04ff4fbcd4789e5382ccc339dbb871192
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-22 10:59:50 +0300

    add new columns to coins table

---
+++ database/migrations/2022_02_22_075413_add_columns_to_coins_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToCoinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('full_name');
+            $table->text('icon_url');
+            $table->unsignedBigInteger('price_usd');
+            $table->text('additional_data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('full_name');
+            $table->dropColumn('icon_url');
+            $table->dropColumn('price_usd');
+            $table->dropColumn('additional_data');
+        });
+    }
+}
