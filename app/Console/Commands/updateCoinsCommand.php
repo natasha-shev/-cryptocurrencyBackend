de4efd4f3ed50bb7f1d7eb1fd484cb0cbfaef391
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-22 21:14:21 +0300

    replace file_get_contents with http client

--- app/Console/Commands/updateCoinsCommand.php
+++ app/Console/Commands/updateCoinsCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Coin;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class updateCoinsCommand extends Command
 {
@@ -39,21 +40,21 @@ class updateCoinsCommand extends Command
     public function handle()
     {
         $url = 'https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=50&tsym=USD';
-        $data = json_decode(file_get_contents($url))->Data;
+        $data = Http::get($url)->json()["Data"];
         foreach ($data as $value) {
-            if (!isset($value->RAW->USD->PRICE)) {
+            if (!isset($value["RAW"]["USD"]["PRICE"])) {
                 continue;
             }
-            $coin = Coin::where('name', $value->CoinInfo->Name)->first();
-            if ($coin==null) {
+            $coin = Coin::where('name', $value["CoinInfo"]["Name"])->first();
+            if (!$coin) {
                 Coin::create([
-                    'name' => $value->CoinInfo->Name,
-                    'full_name' => $value->CoinInfo->FullName,
-                    'icon_url' => 'https://www.cryptocompare.com/' . $value->CoinInfo->ImageUrl,
-                    'price_usd' => $value->RAW->USD->PRICE,
+                    'name' => $value["CoinInfo"]["Name"],
+                    'full_name' => $value["CoinInfo"]["FullName"],
+                    'icon_url' => 'https://www.cryptocompare.com/' . $value["CoinInfo"]["ImageUrl"],
+                    'price_usd' => $value["RAW"]["USD"]["PRICE"],
                 ]);
             } else {
-                $coin->update(['price_usd' => $value->RAW->USD->PRICE]);
+                $coin->update(['price_usd' => $value["RAW"]["USD"]["PRICE"]]);
             }
         }
     }
