738b25a2b10d93dc6a5ea12bcba76a786eaaa26c
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-01 00:37:06 +0300

    fix CoinsController

--- app/Http/Controllers/CoinsController.php
+++ app/Http/Controllers/CoinsController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Coin;
 use Illuminate\Support\Facades\DB;
 
 class CoinsController extends Controller
 {
     public function index(){
         $thisUserId = auth('sanctum')->id();
-        return DB::table('coins')
-            ->leftJoin('user_coin', function($join) use ($thisUserId) {
-                $join->on('user_coin.coin_id', '=', 'coins.id')
-                    ->on('user_coin.user_id', '=', DB::raw($thisUserId));
-
-            })
-            ->get();
+        if ($thisUserId) {
+            return DB::table('coins')
+                ->leftJoin('user_coin', function($join) use ($thisUserId) {
+                    $join->on('user_coin.coin_id', '=', 'coins.id')
+                        ->on('user_coin.user_id', '=', DB::raw($thisUserId));
+                })
+                ->get();
+        } else {
+            return Coin::all();
+        }
     }
 }
