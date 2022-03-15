606d6a19963c705e0ba1a97d794bc082c531b28e
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-15 13:04:50 +0300

    make changes according to new table columns names

--- app/Http/Controllers/FavCoinsController.php
+++ app/Http/Controllers/FavCoinsController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Coin;
 
+use App\Models\PriceHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FavCoinsController extends Controller
 {
@@ -22,13 +24,27 @@ class FavCoinsController extends Controller
         $coin = $request->coinId;
         $user = auth('sanctum')->user();
         return $user->coins()->attach($coin, ['amount' => $request->amount,
-            'bought_on' => $request->buyDate,
-            'purchase_price' => $request->buyPrice]);
+            'date' => $request->date,
+            'price' => $request->price,
+            'action' => $request->action]);
     }
-
 
     public function delete(int $coin_id) {
         $user = auth('sanctum')->user();
         return $user->coins()->detach($coin_id);
+    }
+
+    public function total(Request $request) {
+        $user = auth('sanctum')->user();
+        $coins = $user->coins()->allRelatedIds();
+        return [];
+//          return  PriceHistory::select('*')->whereIn('coin_id', $coins)
+//                ->leftJoin('user_coin', function($join) use ($user) {
+//                    $join->on('coin_id', '=', 'user_coin.coin_id')
+//                        ->on('user_coin.user_id', '=', DB::raw($user->id));
+//                })
+//                ->get();
+
+          // return [smth like 'date', 'total']
     }
 }
